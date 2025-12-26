4e54593e5ff01cb262eb342c0c45f45281265add
shangluz <2975521392@qq.com>
2025-12-26 17:40:40 +0800

    Add files via upload

--- database/connector.py
+++ database/connector.py
@@ -1,7 +1,5 @@
 import pymysql
 from PySide6.QtWidgets import QMessageBox
-from lib.share import SI
-
 
 class Connector:
     __connection = None
@@ -9,42 +7,40 @@ class Connector:
 
     def __init__(self):
         self.__userName = 'root'
-        self.__password = 'root'
-        self.__databaseName = 'bookdb'
+        self.__password = '123456'
+        self.__databaseName = 'transportdb'
         self.__port = 3306
         try:
-            Connector.__connection = pymysql.connect(host='localhost',
-                                                     user=self.__userName,
-                                                     password=self.__password,
-                                                     database=self.__databaseName,
-                                                     port=self.__port)
+            Connector.__connection = pymysql.connect(
+                host='localhost',
+                user=self.__userName,
+                password=self.__password,
+                database=self.__databaseName,
+                port=self.__port
+            )
             Connector.__cursor = Connector.__connection.cursor()
-        except:
-            QMessageBox.critical(SI.g_mainWindow, '错误',
-                                 '数据库链接错误，请检查用户名、密码、端口号和数据库名是否已正确设置')
+        except Exception as e:
+            QMessageBox.critical(None, '错误', f'数据库链接错误: {str(e)}')
 
     @staticmethod
     def get_cursor():
         if Connector.__connection is not None:
-            Connector.__connection.commit()
-            Connector.__cursor.close()
-            Connector.__connection.close()
-        Connector()
-        return Connector.__cursor
+            return Connector.__cursor
+        else:
+            return None
 
     @staticmethod
     def get_connection():
         if Connector.__connection is not None:
-            Connector.__connection.commit()
-            Connector.__cursor.close()
-            Connector.__connection.close()
-        Connector()
-        return Connector.__connection
+            return Connector.__connection
+        else:
+            return None
 
     @staticmethod
     def close_connection():
         if Connector.__cursor is not None:
+            Connector.__cursor.close()
+        if Connector.__connection is not None:
             Connector.__connection.close()
         Connector.__connection = None
         Connector.__cursor = None
-
